bcfe1c22fd90f3c3eb5a7098ccb38594ca5c890e
Harris <hdizdarevic9@gmail.com>
2016-07-07 18:33:40 -0400

    ''

--- GuessingGame/src/core/IClient.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-public interface IClient {
-    void win();
-    void lose();
-    void tooLow(int guess);
-    void tooHigh(int guess);
-}
--- GuessingGame/src/core/IGuesser.java
+++
@@ -1,5 +0,0 @@
-package core;
-
-public interface IGuesser {
-    int nextGuess();
-}
